ee029d2991d4ef0f72b2c5a97dd635e821be3fe9
Crhystel <crhystelvelasco213@gmail.com>
2025-01-07 08:40:49 -0500

    Agregar archivos de proyecto.

---
+++ People/Models/CVPerson.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace People.Models;
+[Table("CVPeople")]
+public class CVPerson
+{
+    [PrimaryKey,AutoIncrement]
+    public int Id { get; set; }
+    [MaxLength(250),Unique]
+    public string Name { get; set; }
+}
